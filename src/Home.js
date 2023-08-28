94430aaf7e9d4e8db32dedf381d3891ff4407887
Nathan-Trust <nathantrustekanem@gmail.com>
2023-08-28 10:28:55 +0100

    first commit

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+  /*     const handleDelete = (id) =>{
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
+     } */
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch(" http://localhost:8000/blogs");
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+      {/* <BlogList blogs={blogs.filter((blog)=> blog.author === "mario")} title="Mario's Blogs"/> */}
+    </div>
+  );
+};
+
+export default Home;
